a74a58992e1e3b6107b9dcd81dbe4c09b7bf0343
Susan Su <515@holbertonschool.com>
2018-10-21 00:30:50 +0000

    added function descriptions

--- print_basic_types.c
+++ print_basic_types.c
@@ -1,5 +1,29 @@
 #include "holberton.h"
 
-int print_char(va_list);
-int print_string(va_list);
-int print_decimal(va_list);
+/**
+  * print_char - prints the single character
+  * @input: argument array that is passed
+  * Return: 1
+  */
+int print_char(va_list input)
+{
+}
+
+/**
+  * print_string - prints the string
+  * @input: argument array that is passed
+  * Return: number of characters
+  */
+int print_string(va_list)
+{
+}
+
+/**
+  * print_decimal - print decimal integer
+  * @input: argument array that is passed
+  * Return: number of digits
+  */
+int print_decimal(va_list)
+{
+
+}
--- printf.c
+++ printf.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
 
+/**
+  * _print - prints the input string
+  * Return: number of printed characters (excluding null byte)
+  */
 int _printf(const char *format, ...)
 {
+
 }
