9ff895b5c6e76b11c6b63a334a6ac4aaa86df0af
christopherchoe <chchchoe@gmail.com>
2018-10-21 06:51:20 +0000

    finished first version of itoa and count_digits

--- utility.c
+++ utility.c
@@ -15,20 +15,56 @@ int _strlen(char *s)
 	return (n);
 }
 
-/** 
+/**
   * _itoa - converts an integer to a string
   * @n: integer to convert
   * Return: converted string
   */
 char *_itoa(int n)
 {
-	int i, digits;
+	int i, d;
 	char *s;
 
+	d = count_digits(n);
+	if (n < 0)
+		d++;
 
-	s = malloc(sizeof(char) * digits);
+	s = malloc(sizeof(char) * d);
 	if (s == NULL)
 		return (NULL);
+
+	d--;
+
+	while (n != 0)
+	{
+		*(s + d) = n % 10 + '0';
+		n = n / 10;
+		d--;
+	}
+
+	if (d != 0)
+	{
+		d--;
+		*(s + d) = '-';
+	}
+
 	return (s);
+}
+
+/**
+  * count_digits - counts the number of digits in a given argument
+  * @n: numeric argument to count digits of
+  * Return: number of digits
+  */
+int count_digits(long int n)
+{
+	int i;
+
+	do {
+		n /= 10;
+		i++;
+	} while (n != 0);
+
+	return (i);
 }
 
