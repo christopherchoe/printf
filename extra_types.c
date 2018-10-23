98640c859c6af2ae05b7c8912dcd2c19ed3e01e8
Susan Su <515@holbertonschool.com>
2018-10-23 16:56:04 +0000

    added conversion of pointer address to long hexadecimal format

--- extra_types.c
+++ extra_types.c
@@ -1,13 +1,40 @@
 #include "holberton.h"
 
+/**
+  * print_address: prints the address of the input
+  * @input: input from variable argument list
+  * Return: number of characters printed
+  */
 int print_address(va_list input)
 {
 	void *storage;
-	unsigned long address;
-	int num_printed;
+	unsigned long int address, result, x, index;
+	unsigned int multiple;
+	unsigned int base = 16;
+	char *hex_values = "0123456789abcdef";
+	char *start = "0x";
 
 	storage = va_arg(input, void *);
 	address = (unsigned long)storage;
-	num_printed = _printf("0x7ffe%x", address);
-	return (num_printed);
+	/* convert pointer address to long hexadecimal format */
+	x = 0;
+	do {
+		result = 15 * power(base, x);
+		x++;
+	} while (address > result);
+	result = address;
+	write(1, start, sizeof(char) * 2);
+	for (index = 0; index < x; index++)
+	{
+		for (multiple = 0; multiple <= base; multiple++)
+		{
+			if (multiple * power(base, x - index - 1) > result)
+			{
+				write(1, hex_values + (--multiple), sizeof(char));
+				break;
+			}
+		}
+		result -= multiple * power(base, x - index - 1);
+	}
+	return (x + 2);
 }
